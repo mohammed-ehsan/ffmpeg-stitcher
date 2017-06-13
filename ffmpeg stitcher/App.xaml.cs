45c9c67ba5d10f8823a60be5da7e1259a5ae4597
Mohammed Ehsan <Mohammed Ehsan@DESKTOP-AEEQ079>
2017-06-13 16:02:24 +0300

    Add project files.

---
+++ ffmpeg stitcher/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace ffmpeg_stitcher
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
