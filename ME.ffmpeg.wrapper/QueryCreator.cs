52179d0bce64a06bac900c61b37a20776377b1ca
Mohammed Ehsan <Mohammed Ehsan@DESKTOP-AEEQ079>
2017-06-13 18:11:25 +0300

    Initial Commit

---
+++ ME.ffmpeg.wrapper/QueryCreator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+
+namespace ME.ffmpeg.wrapper
+{
+    public class QueryCreator
+    {
+        public const string basicCommand =
+            "ffmpeg -r 60 -f image2 -s 1920x1080 -i pic%04d.png -vcodec libx264 -crf 25  -pix_fmt yuv420p test.mp4";
+        private const int _defaultWidth = 1920;
+        private const int _defaultHeight = 1080;
+
+        public string SourcePath { get; private set; }
+        public string NamingConvention { get; private set; }
+        public int FrameRate { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+
+        public QueryCreator(string sourcePath, string NamingConvention)
+        {
+            this.SourcePath = sourcePath;
+            this.NamingConvention = NamingConvention;
+        }
+
+        private string GenerateQuery()
+        {
+            StringBuilder sb = new StringBuilder();
+            //sb.Append("ffmpeg ");
+            if (FrameRate == 0)
+                FrameRate = 30;
+            sb.Append("-r " + FrameRate.ToString() + " ");
+
+            sb.Append("-f image2 ");
+            if (Width > 0 && Height > 0)
+            {
+                sb.Append("-s " + Width + "x" + Height + " ");
+            }
+            else
+            {
+                sb.Append("-s " + _defaultWidth + "x" + _defaultHeight + " ");
+            }
+            sb.Append("-i \"" + SourcePath +"\\"+ NamingConvention + "\" ");
+            sb.Append("-vcodec libx264 -crf 25 -pix_fmt yuv420p ");
+            return sb.ToString();
+        }
+
+        public void RenderVideo(string OutputFile)
+        {
+            string command = GenerateQuery();
+            command = command + "\"" + OutputFile + "\"";
+            if (File.Exists(AppDomain.CurrentDomain.BaseDirectory+"ffmpeg.exe"))
+            {
+                try
+                {
+                    Process.Start(AppDomain.CurrentDomain.BaseDirectory +"ffmpeg.exe", @command);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+            }
+        }
+
+    }
+}
--- ffmpeg stitcher/MainWindow.xaml.cs
+++ ffmpeg stitcher/MainWindow.xaml.cs
@@ -11,17 +11,81 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.ComponentModel;
+using ME.ffmpeg.wrapper;
+using System.IO;
 
 namespace ffmpeg_stitcher
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+
+
+        private string _ImagesSourcePath;
+        public string ImagesSourcePath
+        {
+            get { return _ImagesSourcePath; }
+            set
+            {
+                _ImagesSourcePath = value;
+                OnPropertyChanged("ImagesSourcePath");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void SelectFolder_Click(object sender, RoutedEventArgs e)
+        {
+            using (var ofd = new FolderBrowserDialog())
+            {
+                if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    txtblk_imagesPath.Text = ofd.SelectedPath;
+                    ImagesSourcePath = ofd.SelectedPath;
+                }
+            }
+        }
+
+        private void RenderVideo_Click(object sender, RoutedEventArgs e)
+        {
+            if (Directory.Exists(ImagesSourcePath))
+            {
+                if (!string.IsNullOrEmpty(txtbx_nameingConv.Text))
+                {
+                    QueryCreator cq = new QueryCreator(ImagesSourcePath, txtbx_nameingConv.Text);
+                    try
+                    {
+                        cq.FrameRate = 30;
+                        cq.Height = 1080;
+                        cq.Width = 1920;
+                        Microsoft.Win32.SaveFileDialog sfd = new Microsoft.Win32.SaveFileDialog() { Filter = "mp4 files | *.mp4" };
+                        if (sfd.ShowDialog() == true)
+                        {
+                            cq.RenderVideo(sfd.FileName);
+                            
+                        }
+                    }
+                    catch (Exception)
+                    {
+
+                        throw;
+                    }
+                }
+            }
         }
     }
 }
